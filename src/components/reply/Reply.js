27541c67c6c8ab3f9fc20b9501442e864ef40827
epascua <emily.r.pascua@jpl.nasa.gov>
2019-10-26 15:16:25 -0700

    HEARKEN-01: Added the basic layout to the home page

--- src/components/reply/Reply.js
+++ src/components/reply/Reply.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FaCloud } from 'react-icons/fa';
 
 const Reply = (props) => {
 	return (
 		<div>
-			Reply Component
+			<p className="reply-greeting">Good Morning, How's your day so far?</p>
+			<div className="class">
+				<div className="test"></div>
+				<textarea className="test2" rows="5" cols="50">
+					Test
+				</textarea>
+			</div>
 		</div>
 	)
 }
