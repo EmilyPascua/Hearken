4a9f5e611c1689d526dbf50ee880e2899972c02b
treblegni <ingelbert.figueroa@gmail.com>
2020-06-01 11:20:36 -0700

    removed sensitive information

--- src/fire.js
+++ src/fire.js
@@ -3,14 +3,14 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyD6zJnDuspSlYk8aMkRSrf3-zSHNu6mrPI",
-    authDomain: "hearken-e2343.firebaseapp.com",
-    databaseURL: "https://hearken-e2343.firebaseio.com",
-    projectId: "hearken-e2343",
-    storageBucket: "hearken-e2343.appspot.com",
-    messagingSenderId: "745486021223",
-    appId: "745486021223:web:53b4cb0f3fbe30db611ac2",
-    measurementId: "G-TZM66GNMWC"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 const fire = firebase.initializeApp(config);
 
